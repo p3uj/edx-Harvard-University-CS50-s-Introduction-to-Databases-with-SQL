5866a7ec7eb6ba2c98e0e7385de49084af85dde1
Bengie <bvillesco@gmail.com>
2024-04-19 15:24:36 +0800

    Problem Set 0/cyberchase/9.sql: Add code based on the instruction.

---
+++ Problem Set 0/cyberchase/9.sql
@@ -0,0 +1,2 @@
+-- In 9.sql, write a query that counts the number of episodes released in Cyberchase's first 6 years, from 2002 to 2007, inclusive.
+SELECT COUNT("episode_in_season") AS "number of episodes" FROM "episodes" WHERE "air_date" BETWEEN '2002-01-01' AND '2007-12-31';
