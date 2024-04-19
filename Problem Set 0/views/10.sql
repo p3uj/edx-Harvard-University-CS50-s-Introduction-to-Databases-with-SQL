b23ba92677d495937ddacf5c3c5fec4e9be64a2d
Bengie <bvillesco@gmail.com>
2024-04-19 23:53:06 +0800

    Problem Set 0/views/10.sql: Added code based on the instruction.

---
+++ Problem Set 0/views/10.sql
@@ -0,0 +1,7 @@
+/*
+In 10.sql, write a SQL query to answer a question of your choice about the prints. The query should:
+    - Make use of AS to rename a column
+    - Involve at least one condition, using WHERE
+    - Sort by at least one column, using ORDER BY
+*/
+SELECT SUM("print_number") AS "Total Prints", "english_title", "artist", "print_number" FROM "views" WHERE "artist" = 'Hiroshige' ORDER BY "print_number";
