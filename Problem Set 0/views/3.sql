a50dc5fbd4a571aa27ca3236572cf0e67deabcf9
Bengie <bvillesco@gmail.com>
2024-04-20 10:48:53 +0800

    Problem Set 0/views: Change 1.sql to 10.sql into multiple lines to make it readable.

--- Problem Set 0/views/3.sql
+++ Problem Set 0/views/3.sql
@@ -1,2 +1,4 @@
 -- In 3.sql, write a SQL query to count how many prints by Hokusai include “Fuji” in the English title. Though all of Hokusai's prints focused on Mt. Fuji, in how many did “Fuji” make it into the title?
-SELECT COUNT(*) AS "Number of Fuji Prints" FROM "views" WHERE "artist" = "Hokusai" AND "english_title" LIKE "%Fuji%";
+SELECT COUNT(*) AS "Number of Fuji Prints"
+FROM "views"
+WHERE "artist" = "Hokusai" AND "english_title" LIKE "%Fuji%";
--- Problem Set 0/views/4.sql
+++ Problem Set 0/views/4.sql
@@ -2,4 +2,6 @@
 In 4.sql, write a SQL query to count how many prints by Hiroshige have English titles that refer to the “Eastern Capital”.
 Hiroshige's prints were created in Japan's “Edo period,” referencing the eastern capital city of Edo, now Tokyo.
 */
-SELECT COUNT(*) AS "Number of Eastern Capital Prints" FROM "views" WHERE "artist" = 'Hiroshige' AND "english_title" LIKE '%Eastern Capital%';
+SELECT COUNT(*) AS "Number of Eastern Capital Prints"
+FROM "views"
+WHERE "artist" = 'Hiroshige' AND "english_title" LIKE '%Eastern Capital%';
